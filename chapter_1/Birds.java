f1407210be4218efa64f48db1d01e6a55ef6dfaa
Nematullo Kozimov <koznem24@gmail.com>
2025-11-15 19:09:47 +0100

    Parameter options that can be passed to main method

--- chapter_1/Birds.java
+++ chapter_1/Birds.java
@@ -5,7 +5,16 @@ class Birds{
 }
 
 public class Birds2{
-	public static void main(String... args){
+	// folling are valid parameters for main method:
+	// String[] args
+	// String [] args
+	// String []args
+	// String args[]
+	// String args []
+	// String... args
+	// String ... args
+	// String ...args
+	public static void main(String ...args){
 		System.out.println("Hello"); // Does not compile 
 	}
 
