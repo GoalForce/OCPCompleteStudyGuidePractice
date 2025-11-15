a63a6fca71d77e9717be0ed952a185b60b36c82c
Nematullo Kozimov <koznem24@gmail.com>
2025-11-15 18:55:00 +0100

    .java file can contain multiple classes but only one of them can be public and class name must match file name

---
+++ chapter_1/Birds.java
@@ -0,0 +1,12 @@
+// .java file can contain multipe classes but only one can be declared as public
+// and the one declared public must match file name
+class Birds{
+
+}
+
+public class Birds2{
+	public static void main(String... args){
+		System.out.println("Hello"); // Does not compile 
+	}
+
+}
